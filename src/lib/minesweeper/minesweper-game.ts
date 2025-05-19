83007c49bcac1578651c196b0671d42f68ad0838
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2025-05-19 15:33:24 -0400

    Threw together MinesweeperGame.

---
+++ src/lib/minesweeper/minesweper-game.ts
@@ -0,0 +1,96 @@
+import { type MinesweeperBoard } from './minesweeper-board';
+import { type Position } from './position';
+
+export class MinesweeperGame {
+  private readonly _board: MinesweeperBoard;
+
+  private readonly _states: Record<string, CellState>;
+
+  private constructor(
+    board: MinesweeperBoard,
+    states: Record<string, CellState>
+  ) {
+    this._board = board;
+    this._states = states;
+  }
+
+  public static startNewGame(board: MinesweeperBoard): MinesweeperGame {
+    return new MinesweeperGame(board, {});
+  }
+
+  public get isGameOver(): boolean {
+    return this._board.rows
+      .flat()
+      .some((c) => this.isRevealed(c.position) && c.isBombed);
+  }
+
+  public toggleFlag(position: Position): MinesweeperGame {
+    if (this.isRevealed(position)) {
+      throw new Error(
+        `Cannot toggle flag of position ${position.toString()} which is already revealed.`
+      );
+    }
+
+    if (this.isFlagged(position)) {
+      return this.removeFlag(position);
+    } else {
+      return this.addFlag(position);
+    }
+  }
+
+  private removeFlag(position: Position) {
+    const states: Record<string, CellState> = {
+      ...this._board,
+    };
+
+    delete states[getKey(position)];
+
+    return new MinesweeperGame(this._board, states);
+  }
+
+  private addFlag(position: Position) {
+    const states: Record<string, CellState> = {
+      ...this._board,
+      [getKey(position)]: 'Flagged',
+    };
+
+    return new MinesweeperGame(this._board, states);
+  }
+
+  public reveal(position: Position): MinesweeperGame {
+    if (this.isRevealed(position)) {
+      throw new Error(
+        `Cannot reveal position ${position.toString()} which is already revealed.`
+      );
+    }
+
+    const states: Record<string, CellState> = {
+      ...this._board,
+      [getKey(position)]: 'Revealed',
+    };
+
+    return new MinesweeperGame(this._board, states);
+  }
+
+  public isRevealed(position: Position): boolean {
+    if (this.isGameOver) {
+      return true;
+    }
+
+    return this._states[getKey(position)] === 'Revealed';
+  }
+
+  public isFlagged(position: Position): boolean {
+    if (this.isGameOver) {
+      return false;
+    }
+
+    return this._states[getKey(position)] === 'Flagged';
+  }
+}
+
+type CellState = 'Revealed' | 'Flagged' | undefined;
+
+function getKey(position: Position): string {
+  return `${position.row},${position.column}`;
+}
